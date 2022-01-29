572806270792256719b7b969e6e8bba4d123745f
terra2022devops <shalakhanode@gmail.com>
2022-01-28 21:26:37 -0800

    Added text.

--- c5-outputs.tf
+++ c5-outputs.tf
@@ -2,7 +2,7 @@
 
 # Attribute Reference
 output "ec2_instance_publicip" {
-  description = "EC2 Instance Public IP"
+  description = "EC2 Instance Public IP ----"
   value = aws_instance.my-ec2-vm.*.public_ip
 }
 
